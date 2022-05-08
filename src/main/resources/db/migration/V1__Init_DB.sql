cc58c289077a2ed9f331ecdb731680b9c207d62a
Artem Smirnov <smirnov.art8@gmail.com>
2022-05-08 13:36:23 +0300

    flyway add

---
+++ src/main/resources/db/migration/V1__Init_DB.sql
@@ -0,0 +1,82 @@
+
+CREATE TABLE `captcha_codes` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `code` varchar(255) DEFAULT NULL,
+  `secret_code` varchar(255) DEFAULT NULL,
+  `time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb3;
+
+CREATE TABLE `global_settings` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `code` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `value` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb3;
+
+CREATE TABLE `hibernate_sequence` (
+  `next_val` bigint DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb3;
+
+CREATE TABLE `post_comments` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `parent_id` int DEFAULT NULL,
+  `text` varchar(255) DEFAULT NULL,
+  `time` datetime DEFAULT NULL,
+  `post_id` int DEFAULT NULL,
+  `user_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKaawaqxjs3br8dw5v90w7uu514` (`post_id`),
+  KEY `FKbi7tx0kp7t1pgugyfidm7o00d` (`user_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb3;
+
+CREATE TABLE `post_votes` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `time` datetime DEFAULT NULL,
+  `value` tinyint NOT NULL,
+  `post_id` int DEFAULT NULL,
+  `user_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK9jh5u17tmu1g7xnlxa77ilo3u` (`post_id`),
+  KEY `FKd9rqv0g3hsf0p1bbgre7qjhej` (`user_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb3;
+
+CREATE TABLE `posts` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `is_active` bit(1) DEFAULT NULL,
+  `moderation_status` enum('NEW','ACCEPTED','DECLINED') NOT NULL DEFAULT 'NEW',
+  `moderator_id` int DEFAULT NULL,
+  `text` varchar(255) DEFAULT NULL,
+  `time` datetime DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `view_count` int DEFAULT NULL,
+  `user_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKam8ar6luvp8afhfu20gfsydo9` (`user_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb3;
+
+CREATE TABLE `tag2post` (
+  `post_id` int NOT NULL,
+  `tag_id` int NOT NULL,
+  PRIMARY KEY (`post_id`,`tag_id`),
+  KEY `FKjou6suf2w810t2u3l96uasw3r` (`tag_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb3;
+
+CREATE TABLE `tags` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb3;
+
+CREATE TABLE `user` (
+  `id` int NOT NULL,
+  `code` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `is_moderator` bit(1) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `photo` varchar(255) DEFAULT NULL,
+  `reg_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb3;
